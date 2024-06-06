57fba81a0a4811292e38d9d1a2a21545ea1087a4
Arghya Bhattacharjee <Arghya.Bhattacharjee@tii.ae>
2024-06-06 22:50:16 +0400

    committing

--- p32/src/bank.rs
+++ p32/src/bank.rs
@@ -1,7 +1,9 @@
 pub enum Error {
+    AmountTooLarge,
     SenderNotFound,
     ReceiverNotFound,
     CreditLimitOverflow,
+    BadCreditLimit,
 }
 pub struct User {
     name: String,
@@ -9,7 +11,7 @@ pub struct User {
     balance: i64,
 }
 pub struct Bank {
-    user: Vec<User>,
+    users: Vec<User>,
     name: String,
     credit_interest: u64,
     debit_interest: u64,
@@ -19,7 +21,7 @@ impl Bank {
     pub fn calc_balance(&self) -> (u64, u64) {
         let mut liab: u64 = 0;
         let mut ass: u64 = 0;
-        for i in self.user.iter() {
+        for i in self.users.iter() {
             if i.balance < 0 {
                 ass += i.balance.unsigned_abs();
             } else {
@@ -36,13 +38,13 @@ impl Bank {
         amount: u64,
     ) -> Result<bool, Error> {
         let mut user1_index = usize::MAX;
-        let mut user2_index = self.user.len();
-        let amount_i64 = i64::try_from(amount).unwrap();
+        let mut user2_index = self.users.len();
+        let amount_i64 = i64::try_from(amount).map_err(|_| Error::AmountTooLarge)?;
 
-        for (i, user) in self.user.iter().enumerate() {
+        for (i, user) in self.users.iter().enumerate() {
             if user.name == user1 {
                 user1_index = i;
-                if user2_index != self.user.len() {
+                if user2_index != self.users.len() {
                     break;
                 }
             }
@@ -56,23 +58,23 @@ impl Bank {
         if user1_index == usize::MAX {
             return Err(Error::SenderNotFound);
         }
-        if user2_index == self.user.len() {
+        if user2_index == self.users.len() {
             return Err(Error::ReceiverNotFound);
         }
-        if self.user[user1_index].balance - amount_i64 < 0
-            && (self.user[user1_index].balance - amount_i64).unsigned_abs()
-                > self.user[user1_index].credit_line
-        {
+        let user1_balance = self.users[user1_index].balance;
+        let user1_credit_i64 = i64::try_from(self.users[user1_index].credit_line)
+            .map_err(|_| Error::BadCreditLimit)?;
+        if user1_balance - amount_i64 < -user1_credit_i64 {
             return Err(Error::CreditLimitOverflow);
         }
 
-        self.user[user1_index].balance -= amount_i64;
-        self.user[user2_index].balance += amount_i64;
+        self.users[user1_index].balance -= amount_i64;
+        self.users[user2_index].balance += amount_i64;
         Ok(true)
     }
 
     pub fn accrue_interest(&mut self) {
-        for i in self.user.iter_mut() {
+        for i in self.users.iter_mut() {
             if i.balance > 0 {
                 i.balance +=
                     i64::try_from((i.balance.unsigned_abs() * self.debit_interest) / 10000)
@@ -89,14 +91,14 @@ impl Bank {
         if self.name == new.name {
             return;
         }
-        'outer_loop: for i in new.user {
-            for j in self.user.iter_mut() {
+        'outer_loop: for i in new.users {
+            for j in self.users.iter_mut() {
                 if i.name == j.name {
                     j.balance += i.balance;
                     continue 'outer_loop;
                 }
             }
-            self.user.push(i);
+            self.users.push(i);
         }
     }
 }
