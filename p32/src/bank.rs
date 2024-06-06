3189c49669b3cc728cfb10416bd8145d5fc61e06
Arghya Bhattacharjee <Arghya.Bhattacharjee@tii.ae>
2024-06-06 16:01:24 +0400

    committing

--- p32/src/bank.rs
+++ p32/src/bank.rs
@@ -1,23 +1,17 @@
-#[derive(Clone)]
-pub struct User {
-    pub name: String,
-    pub credit_line: u64,
-    pub balance: i64,
+pub enum Error {
+    Err1,
+    Err2,
 }
-impl Default for User {
-    fn default() -> Self {
-        User {
-            name: "".to_string(),
-            credit_line: 0,
-            balance: 0,
-        }
-    }
+pub struct User {
+    name: String,
+    credit_line: u64,
+    balance: i64,
 }
 pub struct Bank {
-    pub user: Vec<User>,
-    pub name: String,
-    pub credit_interest: u64,
-    pub debit_interest: u64,
+    user: Vec<User>,
+    name: String,
+    credit_interest: u64,
+    debit_interest: u64,
 }
 
 impl Bank {
@@ -33,80 +27,72 @@ impl Bank {
         }
         (liab, ass)
     }
-    pub fn transfer_funds(&mut self, user1: String, user2: String, amount: u64) {
-        let mut temp1 = User::default();
-        let mut temp2 = User::default();
-        let mut flag1: bool = false;
-        let mut flag2: bool = false;
-        for i in self.user.iter() {
-            if i.name == user1 {
-                temp1.name.push_str(i.name.as_str());
-                temp1.credit_line = i.credit_line;
-                temp1.balance = i.balance;
-                flag1 = true;
-            }
-        }
-        for i in self.user.iter() {
-            if i.name == user2 {
-                temp2.name.push_str(i.name.as_str());
-                temp2.credit_line = i.credit_line;
-                temp2.balance = i.balance;
-                flag2 = true;
-            }
-        }
-        if !flag1 || !flag2 {
-            println!("Error: (One or More) Invalid User Names.");
-            return;
-        }
-        if temp1.balance < 0 {
-            if temp1.balance.unsigned_abs() + amount > temp1.credit_line {
-                println!("Error: Invalid Credit Limit.");
-                return;
-            }
-        } else if temp1.balance.unsigned_abs() < amount
-            && amount - temp1.balance.unsigned_abs() > temp1.credit_line
-        {
-            println!("Error: Invalid Credit Limit.");
-            return;
-        }
-        temp1.balance -= i64::try_from(amount).unwrap();
-        temp2.balance += i64::try_from(amount).unwrap();
+    pub fn transfer_funds(
+        &mut self,
+        user1: String,
+        user2: String,
+        amount: u64,
+    ) -> Result<bool, Error> {
+        let mut user1_index = usize::MAX;
+        let mut user2_index = self.user.len();
 
-        for i in self.user.iter_mut() {
-            if i.name == user1 {
-                i.balance = temp1.balance;
+        for (i, user) in self.user.iter().enumerate() {
+            if user.name == user1 {
+                user1_index = i;
+                if user2_index != self.user.len() {
+                    break;
+                }
+            }
+            if user.name == user2 {
+                user2_index = i;
+                if user1_index != usize::MAX {
+                    break;
+                }
             }
         }
-        for i in self.user.iter_mut() {
-            if i.name == user2 {
-                i.balance = temp2.balance;
-            }
+        if user1_index == usize::MAX {
+            return Err(Error::Err1);
         }
+        if user2_index == self.user.len() {
+            return Err(Error::Err1);
+        }
+        if (self.user[user1_index].balance - i64::try_from(amount).unwrap()).unsigned_abs()
+            > self.user[user1_index].credit_line
+        {
+            return Err(Error::Err2);
+        }
+
+        self.user[user1_index].balance -= i64::try_from(amount).unwrap();
+        self.user[user2_index].balance += i64::try_from(amount).unwrap();
+        Ok(true)
     }
 
     pub fn accrue_interest(&mut self) {
         for i in self.user.iter_mut() {
             if i.balance > 0 {
-                i.balance = i.balance
-                    + i64::try_from((i.balance.unsigned_abs() * self.debit_interest) / 10000)
+                i.balance +=
+                    i64::try_from((i.balance.unsigned_abs() * self.debit_interest) / 10000)
                         .unwrap();
             } else {
-                i.balance = i.balance
-                    - i64::try_from((i.balance.unsigned_abs() * self.credit_interest) / 10000)
+                i.balance -=
+                    i64::try_from((i.balance.unsigned_abs() * self.credit_interest) / 10000)
                         .unwrap();
             }
         }
     }
 
     pub fn merge_bank(&mut self, new: Bank) {
-        'outer_loop: for i in new.user.iter() {
+        if self.name == new.name {
+            return;
+        }
+        'outer_loop: for i in new.user {
             for j in self.user.iter_mut() {
                 if i.name == j.name {
                     j.balance += i.balance;
                     continue 'outer_loop;
                 }
             }
-            self.user.push(i.clone());
+            self.user.push(i);
         }
     }
 }
