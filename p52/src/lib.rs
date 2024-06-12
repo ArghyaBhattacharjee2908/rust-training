050d18aa1c061314a1edd243cc89dbc3a78af7fb
Arghya Bhattacharjee <Arghya.Bhattacharjee@tii.ae>
2024-06-12 10:11:26 +0400

    commit thread v1

--- p52/src/lib.rs
+++ p52/src/lib.rs
@@ -3,9 +3,9 @@ const N: usize = 2;
 pub fn map_sum1(vec: Vec<u32>, func: fn(u32) -> u64) -> u64 {
     let vec_len = vec.len();
     let chunk_size = vec_len / N;
-    let mut handles: Vec<std::thread::JoinHandle<u64>> = Vec::new();
     let mut vec_old = vec;
     let mut vec_new;
+    let mut handles: Vec<std::thread::JoinHandle<u64>> = Vec::new();
     for i in 0..(N - 1) {
         vec_new = vec_old.split_off(i * chunk_size);
 
